a178bb429eb766a1a893c883efa0de294f70e4cc
Lars Haulin <lars@haulin.se>
2013-03-07 22:23:53 +0100

    LED now blinks

--- src/main.c
+++ src/main.c
@@ -1,9 +1,39 @@
-#include <stm32f10x_map.h>
+#include "stm32f10x_lib.h"
+
+/* Private variables */
+GPIO_InitTypeDef GPIO_InitStructure;
+ErrorStatus HSEStartUpStatus;
+
+/* Private function prototypes */
+void Delay(vu32 nCount);
+
+/* Private functions */
+void Delay(vu32 nCount)
+{
+  for(; nCount != 0; nCount--);
+}
+
 int main(void)
 {
-	for(;;) {
-		u32 a = GPIOA->CRH;
-		a = (a & ~0xf0) | 0x90;
-		GPIOA->CRH = a;
-	}
+  /* Enable GPIOA clock */
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+
+  /* Configure PA.9 as Output push-pull */
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_10MHz;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+  GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+  while (1)
+  {
+    /* Turn on led connected to PA.9 pin */
+    GPIO_SetBits(GPIOA, GPIO_Pin_9);
+    /* Insert delay */
+    Delay(0xAFFFF);
+
+    /* Turn off led connected to PA.9 pin */
+    GPIO_ResetBits(GPIOA, GPIO_Pin_9);
+    /* Insert delay */
+    Delay(0xAFFFF);
+  }
 }
