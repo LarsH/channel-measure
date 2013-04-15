8f6d61de15740138e10e6a00a0500a0fe1b61fb9
Lars Haulin <lars@haulin.se>
2013-04-15 06:56:02 +0200

    Added command line interface

--- src/main.c
+++ src/main.c
@@ -2,23 +2,65 @@
 #include "hw.h"
 #include "uartio.h"
 
-/* Private variables */
+void handleCommand(char c) {
 
-/* Private function prototypes */
-void Delay(vu32 nCount);
+   switch(c) {
+      case 'a':
+      case 'A':
+         /* Single ADC measurement */
+         printhex(getPower());
+         print("\r\n");
+         break;
 
-/* Private functions */
-void Delay(vu32 nCount)
-{
-   for(; nCount != 0; nCount--);
+      case 'c':
+      case 'C':
+         /* Continous ADC */
+         print("Press any key to stop...\r\n");
+         while(getchar(&c) == 0) {
+            printhex(getPower());
+            print("\r");
+         }
+         print("\n");
+         break;
+
+      case 'H':
+      case 'h':
+      case '?':
+         /* Help */
+         print(
+               "a    Measure power\r\n"
+               "c    Measure power continously\r\n"
+               "h    Show this message\r\n");
+         break;
+
+      default:
+         /* Unknown */
+         print("Unknown command, type 'h' for help.\r\n");
+   }
+}
+
+void commandLine(void) {
+   char buf[4];
+
+   /* Added for command echo on UART */
+   buf[1] = '\r'; buf[2] = '\n'; buf[3] = '\0';
+
+   print("System ready.\r\n");
+
+   /* Infinite command loop */
+   for(;;) {
+      print(">>> ");
+      while(getchar(&(buf[0])) == 0)
+         ;
+
+      print(buf); /* Echo command */
+
+      handleCommand(buf[0]);
+   }
 }
 
 int main(void)
 {
    initHardware();
-   for(;;)
-   {
-      printhex(getPower());
-      print("\r");
-   }
+   commandLine();
 }
